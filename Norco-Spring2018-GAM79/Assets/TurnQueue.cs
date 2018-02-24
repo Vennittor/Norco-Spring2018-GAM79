2a04ffae68758906b37482dc8f7e9f6ba5eee4e7
Unknown <omegazeron@yahoo.com>
2018-02-24 10:50:24 -0800

    Initial commit for turn-taking feature
    Included basic Character classes while waiting on proper ones. Characters are added to a list (will be done by combat manager) and sorted based on speed and amount of turns since last time they went. With current logic, turn order variance stagnates after 2 rounds

---
+++ Norco-Spring2018-GAM79/Assets/TurnQueue.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnQueue : MonoBehaviour {
+
+    public List<Character> characters;
+
+	void Start ()
+    {
+        foreach (Character character in characters)
+        {
+            character.lastTurn = 0;
+        }
+        QueueStart();
+	}
+
+	void Update ()
+    {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            NextTurn();
+        }
+	}
+
+    private int SortBySpeed(Character c1, Character c2)
+    {
+        int char1 = c1.speed + c1.lastTurn;
+        int char2 = c2.speed + c2.lastTurn;
+        if (char1 == char2)
+        {
+            if (c1 is PlayerCharacter && c2 is EnemyCharacter)
+            {
+                return 1;
+            }
+            if (c1 is EnemyCharacter && c2 is PlayerCharacter)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        return -char1.CompareTo(char2);
+    }
+
+    public void QueueStart()
+    {
+        characters.Sort(SortBySpeed);
+    }
+
+    public void NextTurn()
+    {
+        characters[0].lastTurn = 0;
+        characters.Sort(SortBySpeed);
+        foreach (Character character in characters)
+        {
+            character.lastTurn += 1;
+        }
+        Debug.Log(characters[0] + "'s turn.");
+        for (int i = 0; i < characters.Count; i++)
+        {
+            Debug.Log(characters[i] + "Combined: " + (characters[i].speed + characters[i].lastTurn) + ", Speed = " + characters[i].speed + ", lastTurn = " + characters[i].lastTurn);
+        }
+    }
+
+}
