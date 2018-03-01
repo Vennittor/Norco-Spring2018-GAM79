72deafc7c6c69f41a96e11ae178c83888f8a054d
Unknown <aznnoodle101@hotmail.com>
2018-02-28 16:39:18 -0800

    Initial Commit - Attack()
    only committing Attack() function with Debug.Log() to do an initial commit; making sure this pushed correctly

---
+++ Assets/Scripts/Systems/Announcer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Announcer : MonoBehaviour {
+    public void Attack(Character Attacker, Character Defender) {
+        Debug.Log(Attacker.name + " attacks " + Defender.name + "!");
+    }
+}
