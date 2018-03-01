164cb149420879bb16b7cecf134bf95de0852518
Unknown <aznnoodle101@hotmail.com>
2018-02-28 17:05:38 -0800

    basic Announcer functions
    Announcer has:
    Attack()
    TakeDamage()
    UseSkill()
    UseItem()

--- Assets/Scripts/Systems/Announcer.cs
+++ Assets/Scripts/Systems/Announcer.cs
@@ -3,7 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Announcer : MonoBehaviour {
+    // Attack
     public void Attack(Character Attacker, Character Defender) {
         Debug.Log(Attacker.name + " attacks " + Defender.name + "!");
+    }
+
+    // Take Damage
+    public void TakeDamage(Character Defender, float damage) {
+        Debug.Log(Defender.name + " takes " + damage + " damage!");
+    }
+
+    // Use Skill
+    public void UseSkill(PlayerCharacter Player, int skill) {
+        Debug.Log(Player.name + " used Skill #" + skill + "!");
+    }
+
+    // Use Item
+    public void UseItem(PlayerCharacter Player, string item) {
+        Debug.Log(Player.name + " used " + item + "!");
     }
 }
