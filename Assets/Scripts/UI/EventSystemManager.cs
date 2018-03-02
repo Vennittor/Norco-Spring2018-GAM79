bb7ad072b42b48e7e789f52ac09725fe898dc24d
Unknown <stevenxmoreno@gmail.com>
2018-03-02 10:43:59 -0800

    Prototyped possible visual targeting method

---
+++ Assets/Scripts/UI/EventSystemManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystemManager : MonoBehaviour
+{
+	#region Variables
+	public EventSystem eventSystem;
+
+	public BaseRaycaster graphicRaycaster;
+
+	public PointerEventData data;
+	#endregion
+
+	#region Functions
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Mouse0))
+		{
+			OnRaycastHit ();
+		}
+	}
+
+	void OnRaycastHit ()
+	{
+		data = new PointerEventData(eventSystem);
+
+		data.position = Input.mousePosition;
+
+		List<RaycastResult> results = new List<RaycastResult>();
+
+		graphicRaycaster.Raycast(data, results);
+
+		//display rimlight?
+
+		foreach (RaycastResult result in results)
+		{
+			print("Hit " + result.gameObject.name);
+		}
+	}
+	#endregion
+
+}
