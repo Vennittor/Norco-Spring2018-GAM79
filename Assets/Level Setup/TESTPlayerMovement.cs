f1f38844fda8fd9120708b25131c261ddcdf06f1
Scott Etherton <ventor_prime@hotmail.com>
2018-03-06 09:56:22 -0800

    Create inital Level movement Setup Scene with Test Characters
    Created Level Setup folder
    Created TESTLevelSetup Scene
    Created TESTPlayerMovement.cs
    The folder, scene, and other assets in the folder are meant for testing purposes for character movement and other mechanics in Levels.

---
+++ Assets/Level Setup/TESTPlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTPlayerMovement : MonoBehaviour 
+{
+	public float speedForward = 2.0f;
+	public float speedBackward = 2.0f;
+	public float speedUp = 1.0f;
+	public float speedDown = 1.0f;
+
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) 
+		{
+			transform.Translate (Vector3.forward * speedUp * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) 
+		{
+			transform.Translate (Vector3.back * speedDown * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Translate (Vector3.right * speedForward * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Translate (Vector3.left * speedBackward * Time.deltaTime);
+		}
+
+	}
+		
+}
+
