bcfb91ced18b2ad621c9a575cebd58a1153f8529
Unknown <zac.bogner@gmail.com>
2018-03-27 11:20:38 -0700

    Testing ShopUI and scene interaction

--- Assets/Level Design/_Scripts/Test_movement.cs
+++ Assets/Level Design/_Scripts/Test_movement.cs
@@ -2,15 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test_movement : MonoBehaviour {
+public class Test_movement : MonoBehaviour
+{
+    // simple movement system + interaction; 
+    private float playerSpeed = 2.0f;
+    public GameObject player;
+    private Vector3 pos;
+    private bool hasInteracted = false;
+    private bool isInBattle = false;
+    private Rigidbody rB;
+    
+    private void Start()
+    {
+        player = this.gameObject;
+        pos = this.transform.position;
+        hasInteracted = false;
+        isInBattle = false;
+        rB = GetComponent<Rigidbody>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        direction();
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            isInBattle = false;
+            Proceed();
+            rB.AddForce(Vector3.right);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            isInBattle = false;
+            Proceed();
+            rB.AddForce(Vector3.left);
+        }
+
+        
+    }
+
+    private Vector3 direction()
+    {
+        isInBattle = false;
+        if(isInBattle == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("canMove");
+            }
+            Proceed();
+        }
+        else
+        {
+            return pos;
+        }
+
+        float x = transform.position.x * playerSpeed * Time.deltaTime;
+        return pos;
+    }
+
+    private void Proceed()
+    {
+        hasInteracted = true;
+        if(hasInteracted)
+        {
+            float z = transform.position.z * playerSpeed * Time.deltaTime;
+
+        }
+    }
 }
---
+++ Assets/Level Design/_Scripts/itemSelector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class itemSelector : MonoBehaviour
+{
+    public Image imageSelect;
+    public List<Sprite> list = new List<Sprite>();
+    private int itemLoc = 0;
+
+    public void RightArrow()
+    {
+        if(itemLoc < list.Count -1)
+        {
+            itemLoc++;
+            imageSelect.sprite = list[itemLoc];
+        }
+    }
+
+    public void LeftArrow()
+    {
+        if (itemLoc > 0)
+        {
+            itemLoc--;
+            imageSelect.sprite = list[itemLoc];
+        }
+    }
+
+    public void NavigationInventory()
+    {
+
+    }
+}
