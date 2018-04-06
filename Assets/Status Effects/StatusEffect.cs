955b8e148b8e37c425e3c6c73e768d99af158e30
Robert <robby.patterson95@yahoo.com>
2018-04-06 12:57:39 -0700

    Fixed conflicts

--- Assets/Status Effects/StatusEffect.cs
+++ Assets/Status Effects/StatusEffect.cs
@@ -11,4 +11,18 @@ public class StatusEffect: MonoBehaviour
 		Lethargy
 	}
 
+    public void LethargyFunction(int speed)
+    {
+        speed = speed / 2;
+    }
+
+    public void BerserkFunction()
+    {
+        //player can only use ability 1
+    }
+
+    public void StunAbility()
+    {
+        //player looses 1 turn and then has heat reduced
+    }
 }
