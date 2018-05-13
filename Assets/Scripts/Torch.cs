54b80ac7726f87849093d872c675e28ebb8f3ffe
Collin <clowefocal@gmail.com>
2018-05-13 01:13:23 -0700

    Integration and import of clean assets and finished catacombs level.
    Integrated and imported all cleaned and quality tested art assets, including materials, prefab models, textures and particle systems. Finalized catacombs imported.

---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    public float flickerSpeed = 10.0f;
+
+    public float minBrightness = 0.0f;
+    public float maxBrightness = 1.0f;
+
+    public Color color1 = Color.white;
+    public Color color2 = Color.white;
+
+    private Light light = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (light == null)
+            return;
+        
+        light.intensity = Mathf.Lerp(light.intensity, Random.Range(minBrightness, maxBrightness), Time.deltaTime * flickerSpeed);
+
+        light.color = Color.Lerp(light.color, Color.Lerp(color1, color2, Random.RandomRange(0.0f, 1.0f)), Time.deltaTime * flickerSpeed);
+	}
+}
