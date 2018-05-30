b79139b0c7c983821427fd57769dff1bbcce91cd
Unknown <zac.bogner@gmail.com>
2018-05-29 18:49:44 -0700

    Initializing effect for exit transitions

---
+++ Assets/testTransitionEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testTransitionEffect : MonoBehaviour
+{
+    private ParticleSystem pS;
+    public GameObject thisObj;
+    public GameObject targetObj;
+
+    private void Start()
+    {
+        pS = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void StartEmit()
+    {     
+        Emitting();
+    }
+
+    public void Emitting()
+    {
+        pS.transform.SetParent(null);
+        pS.Play();
+    }
+}
